be9015f09abd04458a4d2c8adf757254fe3a9b8d
cnichols-git <cnichols.mail@gmail.com>
2018-09-18 21:15:13 -0500

    add

---
+++ coding/coding.php
@@ -0,0 +1,13 @@
+<?php  include $_SERVER['DOCUMENT_ROOT'] . '/head.php';?>
+<?php  include $_SERVER['DOCUMENT_ROOT'] . '/nav.php';?>
+
+    <div main>
+    	<div class="content pad-1">
+    		<h1 class="text-center mt-2">Coding Examples</h1>
+	    	    <ul class="list-unstyled">
+	      			<li><a href="/coding/code_example.php">Creating DB tables and rows</a></li>
+	    		</ul>
+    	</div>	
+    <div class="footer"><?php include $_SERVER['DOCUMENT_ROOT'] . '/footer.php'?></div>
+  </body>
+</html>
