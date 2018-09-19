f62548c3b2a61f557dc7435560469958163c93e9
cnichols-git <cnichols.mail@gmail.com>
2018-09-18 20:40:09 -0500

    adding linux dir

--- linux/cli_snippets.php
+++ linux/cli_snippets.php
@@ -1,24 +1,47 @@
 <?php  include $_SERVER['DOCUMENT_ROOT'] . '/head.php';?>
 <?php  include $_SERVER['DOCUMENT_ROOT'] . '/nav.php';?>
 
-    <div main>
-    <h2>Title</h2>
+    <div main >
+        <div class="content pad-1">
+            <h1 class="text-center mt-2">CLI Projects</h1>
+            <div class="snippet">
+                <div class="snippet-subject">How to change from Linux/file.php to linux/file.php<br></div>
+                    <p class="snippet-def">
+                        cd /var/www/html<br>#ls<br>Linux<br>#mv /Linux /var/www/html/
+                    </p>
+            </div>
+            <div class="snippet">
+            <div class="snippet-subject">Copy all files in a dir</div>
+                <p class="snippet-def">
+                    cp *.doc /home/tom/backup
+                </p>
+            </div>
+            <div class="snippet">
+            <div class="snippet-subject">How to repair superblock error</div>
+                <p class="snippet-def">
+                    # fsck  /dev/sda2
+                </p>
+            </div>
+            <div class="snippet">
+            <div class="snippet-subject">List numeric version of file permissions</div>
+                <p class="snippet-def">
+                    # stat -c '%a' /etc/passwd
+                </p>
+            </div>
+            <div class="snippet">
+            <div class="snippet-subject">Check software version</div>
+                <p class="snippet-def">
+                    python --version
+                </p>
+            </div>
+        </div>    
     </div>
-
-    body of the page
-
-    here is the first one
-
-    How to change from
-
-    Linux/file.php to linux/file.php
-
-    cd /var/www/html
-    ls
-    Linux
-
-    #mv /Linux /var/www/html/
-
     <div class="footer"><?php include $_SERVER['DOCUMENT_ROOT'] . '/footer.php'?></div>
   </body>
 </html>
+<!--
+<div class="snippet">
+    <div class="snippet-subject"></div>
+    <div class="snippet-def"></div>
+</div>
+-->
